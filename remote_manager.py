91cc5ac61c3dcbd84976b9394e4313b03da9b871
Wojciech Kopanski <wilq.xp@gmail.com>
2021-01-07 15:09:19 +0100

    initial remote_manager.py for PC connectivity

--- remote_manager.py
+++ remote_manager.py
@@ -4,17 +4,18 @@ import tkinter as tk
 import serial  # baud 9600
 
 BANK_NO = 8  # number of banks
+BANK_NAME_LEN = 14  # number of characters in each bank name
 banknames = []  # names of banks
 savebuttons = []  # 
 
-readbytes = ""
+readbytes = "Bank#1        Bank#2        Bank#3  "
 
 
-with serial.Serial() as ser:
-    ser.baudrate = 9600
-    ser.port = "COM5"
-    ser.open()
-    readbytes = ser.read(BANK_NO*14)
+# with serial.Serial() as ser:
+#     ser.baudrate = 9600
+#     ser.port = "COM5"
+#     ser.open()
+#     readbytes = ser.read(BANK_NO*BANK_NAME_LEN)
 
 
 
@@ -27,7 +28,8 @@ window.title("adv IR remote manager")
 name_lbl = tk.Label(text="Bank name")
 
 banknames = [tk.Entry() for i in range(BANK_NO)]
-for i in range(BANK_NO): banknames[i].insert(index=0, string=readbytes[] )
+for i in range(BANK_NO):
+    banknames[i].insert(index=0, string=readbytes[i*BANK_NAME_LEN : (i+1)*BANK_NAME_LEN])
 
 savebuttons = [tk.Button(text="Save") for i in range(BANK_NO)]
 
